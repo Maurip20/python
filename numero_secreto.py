c7126dfd5052e1922a78fa03d8c14fce8fa93299
Maurip20 <maurip20@gmail.com>
2022-01-19 12:37:37 -0300

    agora tem limite de tentativas

--- numero_secreto.py
+++ numero_secreto.py
@@ -1,19 +1,26 @@
 import random
 numero_secreto = random.randint(1,50)
 
-print("Duvido você acertar o número secreto!!!")
 
 
+print("Duvido você acertar o número secreto em 3 tentativas!!!")
+
+tentativas = 0
 numero_escolhido = int(input("Escolha o número: "))
 
-while numero_escolhido != numero_secreto:
-    print("Número errado tente mais uma vez")
+while numero_escolhido != numero_secreto and tentativas < 2 :
+    print("Número errado! Tente mais uma vez")
     if numero_escolhido >= numero_secreto:
         print("O número secreto é menor")
+        tentativas = tentativas + 1
+        print("Resta apenas", 3 - tentativas, "tentativas")
     else:
         print("Número secreto é maior")
+        tentativas = tentativas + 1
+        print("Resta apenas", 3 - tentativas, "tentativas")
     numero_escolhido = int(input("Escolha o número: "))
-    
+
+print(f"Excedeu o número  {tentativas +1} tentativas")
 if numero_escolhido == numero_secreto:
     print(f"Acertou mizeravi, o número secreto é {numero_secreto}")
 

381a8fd1d6cf6d445dd2312d8c20654518803077
Maurip20 <maurip20@gmail.com>
2022-08-26 09:43:51 -0300

    Commit do Calendario em DAX

--- PrimeiroPrograma.py
+++ PrimeiroPrograma.py
@@ -1,4 +1,11 @@
 #Meu Primeiro Programa bora pra cima Programar
 
 
-print("Um texto qualquer")
+#print((1+2)* 5 * 2)
+print("Mauricio" in "Mauricio é o meu manager")
+#Variaveis
+
+nome = input("Qual é o seu nome? \n")
+sobrenome = input("Qual é o seu sobrenome? \n")
+
+print(nome + " " + sobrenome)
